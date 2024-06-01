c419c802981cc7051ba94acdabb36bcfc489a700
jedinrk <jedi.nrk@gmail.com>
2024-06-01 19:43:38 +0530

    setting hardhat project

---
+++ hardhat.config.ts
@@ -0,0 +1,36 @@
+import { HardhatUserConfig, vars } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [vars.get("PVT_KEY_1")]
+    }
+  },
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
+};
+
+export default config;
