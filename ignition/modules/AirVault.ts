a01999d858f24346cdcd59cc2c30b1a725dc7552
jedinrk <jedi.nrk@gmail.com>
2024-06-01 23:31:34 +0530

    AirVault deployment script updated

--- ignition/modules/AirVault.ts
+++ ignition/modules/AirVault.ts
@@ -3,15 +3,15 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 const AirVaultModule = buildModule("AirVaultModule", (m) => {
   const tokenOwner = m.getAccount(0);
 
-  const fudTokenAddress = "";
-  const winTokenAddress = "";
-  const windowSize = 10;
+  const fudTokenAddress = "0xD3b7E82d32D842958E3F01fD9490b57e66bA068F";
+  const winTokenAddress = "0x443161F34026eC906D33D8575a4D69E3332C9181";
+  const blockInterval = 100;
 
   /**Deploying FUDToken */
   const airVault = m.contract("AirVault", [
     fudTokenAddress,
     winTokenAddress,
-    windowSize,
+    blockInterval,
     tokenOwner,
   ]);
 
